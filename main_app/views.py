41c16f8b173e1abb9f24130a2a2cd8db8ac53ed7
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-17 17:41:09 -0400

    retrieves data from api and displays it

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from riotwatcher import LolWatcher, ApiError
+ 
+lol_watcher = LolWatcher('RGAPI-a5e4a44e-5b80-40b4-815b-353384cd7e41')
+my_region = 'na1'
+
+# Getting the latest version of the game from Data Dragon
+versions = lol_watcher.data_dragon.versions_for_region(my_region)
+champions_version = versions['n']['champion']
+# Let's get some champions now
+champion_list = lol_watcher.data_dragon.champions(champions_version)
+champions = champion_list.get('data')
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'champions': champions})
 
 def about(request):
     return render(request, 'about.html')
