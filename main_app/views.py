4b6649abf163ff7f65db4cc8dddf811fb08bc62b
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-17 13:34:05 -0400

    creates home and about templates and renders them

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome Summoner!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>This is the about Page for Summoner Academy</h1>')
+    return render(request, 'about.html')
