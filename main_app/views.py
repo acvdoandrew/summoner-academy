c03a431daeab79ff7f85eb0370b13a5d39c6b2ed
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-18 15:03:02 -0400

    renders builds for each champion in detail page

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from riotwatcher import LolWatcher
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Build
 
  
 lol_watcher = LolWatcher('RGAPI-a5e4a44e-5b80-40b4-815b-353384cd7e41')
@@ -11,7 +12,7 @@ my_region = 'na1'
 versions = lol_watcher.data_dragon.versions_for_region(my_region)
 champions_version = versions['n']['champion']
 # Let's get some champions now
-champion_list = lol_watcher.data_dragon.champions(champions_version)
+champion_list = lol_watcher.data_dragon.champions(champions_version, True)
 champions = champion_list.get('data')
 item_list = lol_watcher.data_dragon.items(champions_version)
 items = item_list.get('data')
@@ -26,8 +27,9 @@ def about(request):
 
 def detail(request, name):
     champ = champions.get(name)
-    print(champ)
-    return render(request, 'detail.html', {'champ': champ})
+    builds = Build.objects.filter(champion=name)
+    print(builds)
+    return render(request, 'detail.html', {'champ': champ, 'builds': builds})
 
 def signup(request):
     error_message = ''
