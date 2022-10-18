7ec1e707ec798a4a8bc7f0b98ed55639e192c65e
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-17 20:41:57 -0400

    creates model and runs migrations to database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Build(models.Model):
+    build_name = models.CharField(max_length=100)
+    mythic = models.CharField(max_length=100)
+    boots = models.CharField(max_length=100)
+    legendary_1 = models.CharField(max_length=100)
+    legendary_2 = models.CharField(max_length=100)
+    legendary_3 = models.CharField(max_length=100)
+    legendary_4 = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.build_name
