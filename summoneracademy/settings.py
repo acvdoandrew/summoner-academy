7d82aa8ea85a30ca805e7d9b9487e6b133b06cf0
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-18 00:07:59 -0400

    sets logout redirect to home

--- summoneracademy/settings.py
+++ summoneracademy/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
