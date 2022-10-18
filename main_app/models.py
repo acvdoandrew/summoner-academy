9f8fd89817e2f181ec1799858400cec348dc446e
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-17 23:56:30 -0400

    adds user field to main model and runs migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Build(models.Model):
@@ -9,6 +10,7 @@ class Build(models.Model):
     legendary_2 = models.CharField(max_length=100)
     legendary_3 = models.CharField(max_length=100)
     legendary_4 = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
 
     def __str__(self):
         return self.build_name
