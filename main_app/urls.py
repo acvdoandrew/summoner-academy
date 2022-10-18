308513f24b1b060b0157a2e69a210e7ddc56619f
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-10-18 00:18:24 -0400

    completes signup feature

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('champions/<str:name>/', views.detail, name='detail'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from riotwatcher import LolWatcher, ApiError
+from django.shortcuts import redirect, render
+from riotwatcher import LolWatcher
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
  
 lol_watcher = LolWatcher('RGAPI-a5e4a44e-5b80-40b4-815b-353384cd7e41')
 my_region = 'na1'
@@ -23,3 +26,17 @@ def detail(request, name):
     champ = champions.get(name)
     print(champ)
     return render(request, 'detail.html', {'champ': champ})
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Signup input invalid - Please try again!'
+    form = UserCreationForm()
+    context = {'form': form, 'error': error_message}
+    return render(request, 'registration/signup.html', context)
