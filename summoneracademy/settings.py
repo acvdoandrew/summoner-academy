d5802498a8fff3c2f0c480fb200cd9f3d1c72602
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-02-26 17:34:59 -0500

    turns on debug

--- summoneracademy/settings.py
+++ summoneracademy/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
